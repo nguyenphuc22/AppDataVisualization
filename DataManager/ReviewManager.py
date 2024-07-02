ab15f80011a8c435d8faf9664fb3a7727d86cd15
PhucVR <59957741+nguyenphuc22@users.noreply.github.com>
2024-07-02 22:51:28 +0700

    Update Singleton For ReviewManager

--- DataManager/ReviewManager.py
+++ DataManager/ReviewManager.py
@@ -6,6 +6,14 @@ class ReviewManager(AbstractDataManager):
         super().__init__()
         self.get_data = None  # or initialize it with some value
 
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(ReviewManager, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def preprocess_data(self, data):
         # Code Tiền Sử Lý Chung Cho Review tại đây nhé
         return data
+
+    def get_data(self):
+        return self.data
