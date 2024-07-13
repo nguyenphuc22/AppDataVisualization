ded73e9067731f6bbf2d5fcec20f4be1678715af
BinhPQ <binhphamvb@gmail.com>
2024-07-13 15:33:14 +0700

    update visual and fix continuesouly input file

--- DataManager/DataManager.py
+++ DataManager/DataManager.py
@@ -26,8 +26,37 @@ class AbstractDataManager(ABC):
         print("Preprocessing data entry Abstract")
         # This method should return a pandas DataFrame
         pass
-    
 
     def update_data(self, new_data):
         new_data.to_excel(self.file_path, index=False)
         print("Data updated successfully.")
+
+def check_format(data):
+    # Convert column names in the DataFrame to lower case
+    data_columns_lower = [col.lower() for col in data.columns]
+    
+    # Define required columns with their lower case equivalents
+    required_product_columns = [
+        'itemid', 'name', 'priceshow', 'discount', 'ratingscore', 'review', 
+        'location', 'sellername', 'sellerid', 'brandname', 'brandid', 
+        'price', 'category', 'originalprice', 'itemsoldcntshow', 'options'
+    ]
+    
+    required_review_columns = [
+        'reviewrateid', 'boughtdate', 'reviewcontent', 'reviewtime', 'rating', 
+        'likecount', 'liketext', 'helpful', 'ispurchased', 'isguest', 
+        'sellerid', 'sellername', 'itemid', 'itemtitle', 'skuinfo', 
+        'skuid', 'upvotes', 'downvotes', 'isgoodreview'
+    ]
+
+    # Check if all required product columns are present (case-insensitive)
+    if all(col in data_columns_lower for col in required_product_columns):
+        return {'valid': True, 'type': 'product'}
+    
+    # Check if all required review columns are present (case-insensitive)
+    elif all(col in data_columns_lower for col in required_review_columns):
+        return {'valid': True, 'type': 'review'}
+    
+    # If neither condition is met, return invalid
+    else:
+        return {'valid': False}
